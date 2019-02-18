e37627b6027806d0c859a2346f16198aa0877c51
TheParodicts <brianrodrig@gmail.com>
2019-02-18 13:28:00 -0400

    mergin branch up

--- config/dbconfig.py
+++
@@ -1,8 +0,0 @@
-#Databse configuration information
-
-pg_config = {
-    'user' : 'enter_db_username_here',
-    'passwd' : 'enter_db_password_here',
-    'dbname' : 'enter_db_name_here',
-    'host': '127.0.0.1'
-}
