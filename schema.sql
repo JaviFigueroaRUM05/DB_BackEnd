284ef4c334950aee33dd28823b57d4fe2af6765c
TheParodicts <brianrodrig@gmail.com>
2019-03-13 10:15:21 -0400

    Implemented Proto-Schema to create Proto-tables.
    Moved the Proto-schema to an sql file and reorganized the queries so that they can be properly created in a non-conflicting order. TODO: Add appropriate test records to all tables.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,36 @@
+-- Actual Project schema
+--
+Create table Person(pid serial primary key, first_name varchar (20), last_name varchar (20),
+                    email varchar (40), phone integer);
+
+Create table Users(uid serial primary key, pid integer references Person(pid), uname varchar (20),
+                    password varchar (20));
+
+Create table Contacts(uid integer references Users(uid), cid integer references Users(uid), primary key (uid, cid));
+
+Create table Cgroup(gid serial primary key, gName varchar (20), gPhoto varchar (100));
+
+Create table Admins(uid integer references Users(uid), gid integer references Cgroup(gid), primary key (uid,gid));
+
+Create table Participants(uid integer references Users(uid), gid integer references Cgroup(gid), primary key (uid,gid));
+
+Create table Post(postID serial primary key, pDate timestamp, message text, media text,
+                    uid integer references Users(uid), gid integer references Cgroup(gid));
+
+Create table Reaction(rid serial primary key, rDate timestamp, rType char (1), postID integer references Post(postID));
+
+Create table Reacts(uid integer references Users(uid), rid integer references Reaction(rid), primary key (uid, rid));
+
+Create table Replies(postID integer references Post(postID), replyID integer references Post(postID),
+                     primary key (postID, replyID));
+
+Create table Hashtag(hid serial primary key, hName varchar(20));
+
+Create table Tagged(postID integer references Post(postID), hid integer references Hashtag(hid),
+                    primary key (postID, hid));
+
+
+/*
 -- This file contains the definitions of the tables to be used in the application.
 --
 create table chat_groups(gid serial primary key, gname varchar(20), gphoto varchar(200), gadmin integer references users(uid));
@@ -25,4 +58,4 @@ CREATE TABLE public.participants
 create table Reply(rid serial primary key, original_post integer references Post(pid), reply_post integer references Post(pid))
 
 create table Group_Posts(gp_id serial primary key, gid integer references chat_groups(gid), pid integer references post(pid))
-
+*/
