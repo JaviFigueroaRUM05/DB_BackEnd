cf2e303a2c7b42d20f2eb4dc7133f9b2f1bf52fa
TheParodicts <brianrodrig@gmail.com>
2019-03-17 19:46:05 -0400

    Updated Proto-Schema and added a Schema filler
    Updated the Proto-schema by:
    -Changing Person(phone) to varchar.
    -Adding NOT NULL and UNIQUE constraints for columns.
    -Made sure Contacts(uid != cid) (contact cannot be its own contact).
    -Commented on how to create the dependency that Admins must be participants of their group.
    
    Implemented an Sql scritp that fills all tables with at least one record of data appropriately. (there's 4 hours of my life gone!)

--- schema.sql
+++ schema.sql
@@ -1,33 +1,35 @@
 -- Actual Project schema
 --
-Create table Person(pid serial primary key, first_name varchar (20), last_name varchar (20),
-                    email varchar (40), phone integer);
+Create table Person(pid serial primary key, first_name varchar (20) NOT NULL, last_name varchar (20)NOT NULL,
+                    email varchar (40) NOT NULL UNIQUE, phone varchar(11) NOT NULL UNIQUE);
 
-Create table Users(uid serial primary key, pid integer references Person(pid), uname varchar (20),
-                    password varchar (20));
+Create table Users(uid serial primary key, pid integer references Person(pid) NOT NULL, uname varchar (20) NOT NULL UNIQUE,
+                    password varchar (20) NOT NULL);
 
-Create table Contacts(uid integer references Users(uid), cid integer references Users(uid), primary key (uid, cid));
+Create table Contacts(uid integer references Users(uid) NOT NULL, cid integer references Users(uid) NOT NULL CHECK (cid <> uid), primary key (uid, cid));
 
-Create table Cgroup(gid serial primary key, gName varchar (20), gPhoto varchar (100));
+Create table Cgroup(gid serial primary key, gName varchar (20) NOT NULL, gPhoto varchar (100));
 
-Create table Admins(uid integer references Users(uid), gid integer references Cgroup(gid), primary key (uid,gid));
+--How can we ensure that all admins are participants in their own chats?
+Create table Admins(uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL, primary key (uid,gid));
 
-Create table Participants(uid integer references Users(uid), gid integer references Cgroup(gid), primary key (uid,gid));
+Create table Participants(uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL, primary key (uid,gid));
 
-Create table Post(postID serial primary key, pDate timestamp, message text, media text,
-                    uid integer references Users(uid), gid integer references Cgroup(gid));
+Create table Post(postID serial primary key, pDate timestamp NOT NULL, message text, media text,
+                    uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL);
 
-Create table Reaction(rid serial primary key, rDate timestamp, rType char (1), postID integer references Post(postID));
+Create table Reaction(rid serial primary key, rDate timestamp NOT NULL, rType char (1) NOT NULL, postID integer references Post(postID) NOT NULL);
 
-Create table Reacts(uid integer references Users(uid), rid integer references Reaction(rid), primary key (uid, rid));
+Create table Reacts(uid integer references Users(uid) NOT NULL, rid integer references Reaction(rid) NOT NULL, primary key (uid, rid));
 
-Create table Replies(postID integer references Post(postID), replyID integer references Post(postID),
+Create table Replies(postID integer references Post(postID) NOT NULL, replyID integer references Post(postID) NOT NULL,
                      primary key (postID, replyID));
 
-Create table Hashtag(hid serial primary key, hName varchar(20));
+Create table Hashtag(hid serial primary key, hName varchar(20) NOT NULL UNIQUE);
 
-Create table Tagged(postID integer references Post(postID), hid integer references Hashtag(hid),
+Create table Tagged(postID integer references Post(postID) NOT NULL, hid integer references Hashtag(hid) NOT NULL,
                     primary key (postID, hid));
+
 
 
 /*
---
+++ schema_filler.sql
@@ -0,0 +1,171 @@
+-- Fill in test data for schema
+--Persons
+insert into person(first_name, last_name, email, phone) VALUES( 'Brian' , 'Rodriguez', 'brianrodrig@gmail.com', '7879067012');
+insert into person(first_name, last_name, email, phone) VALUES( 'Sofia' , 'Saavedra', 'sofia.saavedra@gmail.com', '7876666969');
+insert into person(first_name, last_name, email, phone) VALUES( 'Javier' , 'Figueroa', 'javier.figueroa29@upr.edu', '7879637512');
+insert into person(first_name, last_name, email, phone) VALUES( 'Manuel' , 'Rodriguez', 'manuel.rodriguez@gmail.com', '9395874632');
+insert into person(first_name, last_name, email, phone) VALUES( 'Michael' , 'Jackson', 'ayuwoki@gmail.com', '8459638521');
+insert into person(first_name, last_name, email, phone) VALUES( 'Michael' , 'Buble', 'mbuble@yahoo.com', '7974521683');
+insert into person(first_name, last_name, email, phone) VALUES( 'Eddie' , 'VanHalen', 'hotforteacher@gmail.com', '4512222222');
+insert into person(first_name, last_name, email, phone) VALUES( 'Harvey' , 'Weinstein', 'metoo@gmail.com', '9116969696');
+insert into person(first_name, last_name, email, phone) VALUES( 'Leonard' , 'Neemoy', 'spock@gmail.com', '0101010101');
+insert into person(first_name, last_name, email, phone) VALUES( 'Tom' , 'Frommyspace', 'tom@myspace.com', '9394561248');
+
+--Users
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='brianrodrig@gmail.com'),'TheParodict','admin');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='sofia.saavedra@gmail.com'),'Hydrogon','admin');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='javier.figueroa29@upr.edu'),'Javi','admin');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='manuel.rodriguez@gmail.com'),'ManuelDB','sinsabores');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='ayuwoki@gmail.com'),'SmoothCriminal','heehee');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='mbuble@yahoo.com'),'MB','feelingood');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='hotforteacher@gmail.com'),'EVH','eruption');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='metoo@gmail.com'),'Hollywoodperv','#metoo');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='spock@gmail.com'),'Vulcan','livelong&prosper');
+insert into users(pid, uname, password) VALUES(
+    (select pid from person where email='tom@myspace.com'),'tom','screwfacebook');
+
+--Contacts (only TheParodict, Hydrogon, and Javi have contacts)
+insert into contacts(uid, cid) VALUES((select uid from users where uname='TheParodict'),
+    (select uid from users where uname='Hydrogon'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='TheParodict'),
+    (select uid from users where uname='MB'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='TheParodict'),
+    (select uid from users where uname='Javi'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='TheParodict'),
+    (select uid from users where uname='EVH'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Hydrogon'),
+    (select uid from users where uname='TheParodict'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Hydrogon'),
+    (select uid from users where uname='Vulcan'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Hydrogon'),
+    (select uid from users where uname='SmoothCriminal'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Javi'),
+    (select uid from users where uname='Hydrogon'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Javi'),
+    (select uid from users where uname='Hollywoodperv'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Javi'),
+    (select uid from users where uname='tom'));
+insert into contacts(uid, cid) VALUES((select uid from users where uname='Javi'),
+    (select uid from users where uname='ManuelDB'));
+
+--Groups
+insert into cgroup(gname)VALUES('The first chat');
+insert into cgroup(gname)VALUES('Admins');
+insert into cgroup(gname)VALUES('trambolico');
+
+--Group admins
+insert into admins(uid, gid) VALUES((select uid from users where uname='TheParodict'),
+    (select gid from cgroup where gname='The first chat'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='TheParodict'),
+    (select gid from cgroup where gname='Admins'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='Javi'),
+    (select gid from cgroup where gname='Admins'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='Hydrogon'),
+    (select gid from cgroup where gname='Admins'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='TheParodict'),
+    (select gid from cgroup where gname='trambolico'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='Javi'),
+    (select gid from cgroup where gname='trambolico'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='Hydrogon'),
+    (select gid from cgroup where gname='trambolico'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='ManuelDB'),
+    (select gid from cgroup where gname='trambolico'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='tom'),
+    (select gid from cgroup where gname='trambolico'));
+insert into admins(uid, gid) VALUES((select uid from users where uname='EVH'),
+    (select gid from cgroup where gname='trambolico'));
+
+--Group Participants (how do we make sure admins are participants?)
+insert into participants(uid, gid) VALUES((select uid from users where uname='TheParodict'),
+    (select gid from cgroup where gname='The first chat'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Hydrogon'),
+    (select gid from cgroup where gname='The first chat'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='TheParodict'),
+    (select gid from cgroup where gname='Admins'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Hydrogon'),
+    (select gid from cgroup where gname='Admins'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Javi'),
+    (select gid from cgroup where gname='Admins'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='TheParodict'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Hydrogon'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Javi'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='ManuelDB'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='tom'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='EVH'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='MB'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Vulcan'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='SmoothCriminal'),
+    (select gid from cgroup where gname='trambolico'));
+insert into participants(uid, gid) VALUES((select uid from users where uname='Hollywoodperv'),
+    (select gid from cgroup where gname='trambolico'));
+
+--Posts
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:40:00','Hello world',
+    (select uid from users where uname='TheParodict'), (select gid from cgroup where gname='The first chat'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:41:00','Cactus',
+    (select uid from users where uname='Hydrogon'), (select gid from cgroup where gname='The first chat'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:42:00','When does the Narwhal Bacon?',
+    (select uid from users where uname='TheParodict'), (select gid from cgroup where gname='The first chat'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:43:00','Midnight!',
+    (select uid from users where uname='TheParodict'), (select gid from cgroup where gname='The first chat'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:40:00','Hello my Admins!',
+    (select uid from users where uname='TheParodict'), (select gid from cgroup where gname='Admins'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:41:00','DB is my favorite Class!',
+    (select uid from users where uname='Javi'), (select gid from cgroup where gname='Admins'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:42:00','Im drowning in my classes #pleasehelp', --#
+    (select uid from users where uname='Hydrogon'), (select gid from cgroup where gname='Admins'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:43:00','LOL!',
+    (select uid from users where uname='Javi'), (select gid from cgroup where gname='Admins'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:40:00','Hello!',
+    (select uid from users where uname='TheParodict'), (select gid from cgroup where gname='trambolico'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:41:00','Back from the dead to touch more kids!',
+    (select uid from users where uname='SmoothCriminal'), (select gid from cgroup where gname='trambolico'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:42:00','Dude, wtf!',
+    (select uid from users where uname='Hollywoodperv'), (select gid from cgroup where gname='trambolico'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:43:00','All this talk of sensuality seems illogical.',
+    (select uid from users where uname='Vulcan'), (select gid from cgroup where gname='trambolico'));
+insert into post(pdate, message, uid, gid) VALUES('2019-03-17 18:44:00','Notice me, senpai!',
+    (select uid from users where uname='Hydrogon'), (select gid from cgroup where gname='trambolico'));
+
+--Replies
+insert into replies(postid, replyid) VALUES((select postid from post where message='Midnight!'),
+(select postid from post where  message='When does the Narwhal Bacon?'));
+insert into replies(postid, replyid) VALUES((select postid from post where message='LOL!'),
+(select postid from post where message='Im drowning in my classes #pleasehelp'));
+insert into replies(postid, replyid) VALUES((select postid from post where message='Dude, wtf!'),
+(select postid from post where message='Back from the dead to touch more kids!'));
+insert into replies(postid, replyid) VALUES((select postid from post where message='Notice me, senpai!'),
+(select postid from post where message='All this talk of sensuality seems illogical.'));
+
+--Hashtags
+insert into hashtag(hname) VALUES('#pleasehelp');
+
+--Tagged
+insert into tagged(postid, hid) VALUES((select postid from post where message='Im drowning in my classes #pleasehelp'),
+(select hid from hashtag where hname='#pleasehelp'));
+
+--Reactions
+insert into reaction (rdate, rtype, postid) VALUES('2019-03-17 18:43:00', 'L',
+    (select postid from post where message='Im drowning in my classes #pleasehelp'));
+
+--Reacts
+insert into reacts(uid, rid) VALUES((select uid from users where uname='Javi'),
+    (select rid from reaction where rdate='2019-03-17 18:43:00'));
+
