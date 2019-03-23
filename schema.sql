febff9efba03a196e04613f4ad92aadb65e26e37
TheParodicts <brianrodrig@gmail.com>
2019-03-23 14:43:58 -0400

    Modified Proto-Schema to align with modified E-R
    -Removed Persons
    -Integrated persons into Users
    -Integrated Admins and Participants into Admins
    -Added mediaType to Post
    -Renamed Posts to Authored

--- schema.sql
+++ schema.sql
@@ -1,29 +1,33 @@
 -- Actual Project schema
 --
-Create table Person(pid serial primary key, first_name varchar (20) NOT NULL, last_name varchar (20)NOT NULL,
-                    email varchar (40) NOT NULL UNIQUE, phone varchar(11) NOT NULL UNIQUE);
+--Create table Person(pid serial primary key, first_name varchar (20) NOT NULL, last_name varchar (20)NOT NULL,
+--                    email varchar (40) NOT NULL UNIQUE, phone varchar(11) NOT NULL UNIQUE);
 
-Create table Users(uid serial primary key, pid integer references Person(pid) NOT NULL, uname varchar (20) NOT NULL UNIQUE,
-                    password varchar (20) NOT NULL);
+Create table Users(uid serial primary key, uname varchar (20) NOT NULL UNIQUE,
+                    password varchar (20) NOT NULL, first_name varchar (20) NOT NULL,
+                     last_name varchar (20)NOT NULL,
+                    email varchar (40) NOT NULL UNIQUE, phone varchar(11) NOT NULL UNIQUE);
 
 Create table Contacts(uid integer references Users(uid) NOT NULL, cid integer references Users(uid) NOT NULL CHECK (cid <> uid), primary key (uid, cid));
 
 Create table Cgroup(gid serial primary key, gName varchar (20) NOT NULL, gPhoto varchar (100));
 
 --How can we ensure that all admins are participants in their own chats?
-Create table Admins(uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL, primary key (uid,gid));
+--Create table Admins(uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL, primary key (uid,gid));
 
-Create table Participants(uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL, primary key (uid,gid));
+Create table Participants(uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL,
+                            primary key (uid,gid), isAdmin boolean NOT NULL);
 
-Create table Post(postID serial primary key, pDate timestamp NOT NULL, message text, media text,
+Create table Post(postID serial primary key, pDate timestamp NOT NULL, message text,
+                mediaType character[1] NOT NULL,  media text,
                     uid integer references Users(uid) NOT NULL, gid integer references Cgroup(gid) NOT NULL);
 
 Create table Reaction(rid serial primary key, rDate timestamp NOT NULL, rType char (1) NOT NULL, postID integer references Post(postID) NOT NULL);
 
 Create table Reacts(uid integer references Users(uid) NOT NULL, rid integer references Reaction(rid) NOT NULL, primary key (uid, rid));
 
-Create table Replies(postID integer references Post(postID) NOT NULL, replyID integer references Post(postID) NOT NULL,
-                     primary key (postID, replyID));
+Create table Replies(opID integer references Post(postID) NOT NULL, replyID integer references Post(postID) NOT NULL,
+                     primary key (opID, replyID));
 
 Create table Hashtag(hid serial primary key, hName varchar(20) NOT NULL UNIQUE);
 
